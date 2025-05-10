a8207005eb4eda8e20833445c1c0bbd609861940
youseffm10 <99771155+youseffm10@users.noreply.github.com>
2025-05-10 17:54:35 -0400

    Create ArduinoLedServoMotor.java

---
+++ src/ArduinoLedServoMotor.java
@@ -0,0 +1,17 @@
+
+package arduinoledservomotor;
+
+/**
+ *
+ * @author amine gasa
+ */
+public class ArduinoLedServoMotor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
